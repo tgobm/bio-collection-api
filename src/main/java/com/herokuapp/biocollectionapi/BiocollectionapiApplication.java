d10b7fc0d3ff26a1dbf363c56b683baddb750dec
Tiago Boaventura Miranda <98120297+tgobm@users.noreply.github.com>
2022-02-18 20:34:23 -0300

    feat: initial files

---
+++ src/main/java/com/herokuapp/biocollectionapi/BiocollectionapiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.biocollectionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiocollectionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiocollectionapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/biocollectionapi/BiocollectionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.biocollectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiocollectionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
